f42292c063919409384ac75a39c20941e84b6e17
Harmanjit Singh <harmanjit140500@gmail.com>
2020-12-19 13:50:48 +0530

    auth done

--- oneness/lib/main.dart
+++ oneness/lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-
+import 'pages/auth.dart';
 import 'pages/spash.dart';
 
 void main() {
@@ -10,13 +10,17 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'OneNess',
       theme: ThemeData(
         primarySwatch: Colors.green,
         visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
-      home: SplashScreen()
+      routes: {
+        "/": (context) => SplashScreen(),
+        "/auth": (context) => Auth(),
+      },
+      initialRoute: "/auth",
     );
   }
 }
-
--- oneness/lib/pages/spash.dart
+++ oneness/lib/pages/spash.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class SplashScreen extends StatefulWidget {
   @override
@@ -8,12 +9,39 @@ class SplashScreen extends StatefulWidget {
 class _SplashScreenState extends State<SplashScreen> {
   @override
   Widget build(BuildContext context) {
+    var size = MediaQuery.of(context).size;
     return Scaffold(
       body: SafeArea(
         child: Container(
+          width: size.width,
+          height: size.height,
           margin: EdgeInsets.all(20),
           child: Column(
-            children: [],
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Container(
+                  child: Text(
+                "OneNess",
+                style: GoogleFonts.poppins(
+                  textStyle: TextStyle(
+                    fontSize: 50,
+                    color: Colors.black,
+                    fontWeight: FontWeight.bold,
+                    shadows: <Shadow>[
+                      Shadow(
+                        offset: Offset(1, 0.5),
+                        blurRadius: 3,
+                        color: Color.fromARGB(255, 0, 0, 0),
+                      ),
+                    ],
+                  ),
+                ),
+              )),
+              SizedBox(height: 20),
+              Container(
+                child: Image.asset("hands.gif"),
+              ),
+            ],
           ),
         ),
       ),

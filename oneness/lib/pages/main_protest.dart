fba1d1cf0d9c76d32823fcba04d266ed1c870f5e
Harmanjit Singh <harmanjit140500@gmail.com>
2020-12-19 16:32:22 +0530

    30% Done

--- oneness/lib/pages/main_protest.dart
+++ oneness/lib/pages/main_protest.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_spinkit/flutter_spinkit.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:google_fonts/google_fonts.dart';
 
 class MainProtest extends StatefulWidget {
@@ -8,10 +10,91 @@ class MainProtest extends StatefulWidget {
 
 class _MainProtestState extends State<MainProtest> {
   int dropVal = 0;
+  int stateBut = 0;
+
+  Widget logChild() {
+    switch (stateBut) {
+      case 0:
+        {
+          return Text(
+            "Continue",
+            style: GoogleFonts.poppins(
+                textStyle: TextStyle(fontSize: 20, color: Colors.white)),
+          );
+        }
+        break;
+      case 1:
+        {
+          return SpinKitDoubleBounce(
+            color: Colors.white,
+            size: 30.0,
+          );
+        }
+        break;
+      case 2:
+        {
+          return Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Icon(
+                FontAwesomeIcons.checkCircle,
+                color: Colors.white,
+              ),
+              SizedBox(
+                width: 7,
+              ),
+              Text(
+                "Success",
+                style: GoogleFonts.poppins(
+                    textStyle: TextStyle(
+                        fontSize: 20,
+                        color: Colors.white,
+                        fontWeight: FontWeight.bold)),
+              ),
+            ],
+          );
+        }
+        break;
+      case 3:
+        {
+          return Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Icon(
+                FontAwesomeIcons.timesCircle,
+                color: Colors.white,
+              ),
+              SizedBox(
+                width: 7,
+              ),
+              Text(
+                "Check Credentials!",
+                style: GoogleFonts.poppins(
+                    textStyle: TextStyle(
+                        fontSize: 20,
+                        color: Colors.white,
+                        fontWeight: FontWeight.bold)),
+              ),
+            ],
+          );
+        }
+        break;
+      default:
+        {
+          return Text(
+            "Continue",
+            style: GoogleFonts.poppins(
+                textStyle: TextStyle(fontSize: 20, color: Colors.white)),
+          );
+        }
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
+        backgroundColor: Colors.brown,
         actions: [IconButton(icon: Icon(Icons.logout), onPressed: () {})],
         title: Text(
           "Choose your Protest",
@@ -24,53 +107,116 @@ class _MainProtestState extends State<MainProtest> {
       ),
       backgroundColor: Colors.white,
       body: Container(
-        margin: EdgeInsets.all(20),
         child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          mainAxisSize: MainAxisSize.max,
           children: [
-            Container(
-              child: DropdownButton(
-                icon: Icon(Icons.ac_unit),
-                dropdownColor: Colors.green,
-                value: dropVal,
-                items: [
-                  DropdownMenuItem(
-                    child: Text("Farmers"),
-                    value: 0,
-                    onTap: () {
-                      setState(() {});
-                    },
-                  ),
-                  DropdownMenuItem(
-                    child: Text("Farmers 2"),
-                    value: 3,
-                    onTap: () {
-                      setState(() {});
-                    },
-                  ),
-                  DropdownMenuItem(
-                    child: Text("Farmers 3"),
-                    value: 1,
-                    onTap: () {
-                      setState(() {});
-                    },
-                  ),
-                  DropdownMenuItem(
-                    child: Text("Farmers 4"),
-                    value: 2,
-                    onTap: () {
-                      setState(() {});
-                    },
-                  )
-                ],
-                onChanged: (int value) {
-                  dropVal = value;
-                  setState(() {});
-                },
+            Expanded(
+              child: Container(
+                child: Image.asset("people2.gif"),
               ),
             ),
-            Container(
-              child: Image.asset("people2.gif"),
-            )
+            Column(
+              children: [
+                Container(
+                  decoration: BoxDecoration(
+                      boxShadow: [
+                        new BoxShadow(
+                          blurRadius: 3.0,
+                        ),
+                      ],
+                      color: Colors.grey[200],
+                      borderRadius: BorderRadius.circular(40)),
+                  margin: EdgeInsets.fromLTRB(20, 20, 20, 0),
+                  alignment: Alignment.center,
+                  padding: EdgeInsets.all(15),
+                  width: MediaQuery.of(context).size.width,
+                  child: DropdownButton(
+                    icon: Icon(FontAwesomeIcons.angleDown),
+                    iconSize: 27,
+                    iconEnabledColor: Colors.black,
+                    hint: Text("Select one to start!"),
+                    isDense: false,
+                    style:
+                        GoogleFonts.poppins(fontSize: 25, color: Colors.black),
+                    underline: SizedBox(),
+                    elevation: 0,
+                    dropdownColor: Colors.grey[200],
+                    value: dropVal,
+                    items: [
+                      DropdownMenuItem(
+                        child: Container(child: Text("Farmers")),
+                        value: 0,
+                        onTap: () {
+                          setState(() {});
+                        },
+                      ),
+                      DropdownMenuItem(
+                        child: Text("Farmers 2"),
+                        value: 3,
+                      ),
+                      DropdownMenuItem(
+                        child: Text("Farmers 3"),
+                        value: 1,
+                      ),
+                      DropdownMenuItem(
+                        child: Text("Farmers 4"),
+                        value: 2,
+                      )
+                    ],
+                    onChanged: (int value) {
+                      dropVal = value;
+                      setState(() {});
+                    },
+                  ),
+                ),
+                Container(
+                  width: double.maxFinite,
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(40),
+                    boxShadow: [
+                      new BoxShadow(
+                        blurRadius: 1.0,
+                      ),
+                    ],
+                  ),
+                  margin: EdgeInsets.fromLTRB(20, 15, 20, 30),
+                  child: ClipRRect(
+                    borderRadius: BorderRadius.circular(40),
+                    child: RaisedButton(
+                      padding: EdgeInsets.all(15),
+                      onPressed: () {},
+                      //  () async {
+                      //   stateBut = 1;
+                      //   setState(() {});
+                      //   int temp = 0; //await getToken();
+                      //   if (temp == 1) {
+                      //     Timer(Duration(seconds: 2), () {
+                      //       stateBut = 2;
+                      //       setState(() {});
+                      //     });
+                      //     Timer(Duration(seconds: 3), () {
+                      //       password = "";
+                      //       Navigator.pushReplacementNamed(context, "/nav");
+                      //     });
+                      //   } else {
+                      //     Timer(Duration(seconds: 2), () {
+                      //       stateBut = 3;
+                      //       setState(() {});
+                      //     });
+                      //     Timer(Duration(seconds: 4), () {
+                      //       stateBut = 0;
+                      //       setState(() {});
+                      //     });
+                      //   }
+                      // },
+                      child: logChild(),
+                      color: Colors.brown,
+                    ),
+                  ),
+                ),
+              ],
+            ),
           ],
         ),
       ),
